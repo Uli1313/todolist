a112042b770a1864dfa2837be0e37dcca5eb6251
Uli <ru21041555@gmail.com>
2023-10-29 21:56:02 +0800

    修改路徑

--- all.js
+++ all.js
@@ -1,16 +1,14 @@
 const txt = document.querySelector(".txt");
 const save = document.querySelector(".save");
 const list = document.querySelector(".list");
-const _url = "https://todolist-20of.onrender.com/"; // 修改處
+const _url = "https://todolist-20of.onrender.com"; // 修改處
 let data = [];
 function init() {
-  axios
-    .get(`https://todolist-20of.onrender.com/todos`)
-    .then(function (response) {
-      data = response.data;
-      console.log(data);
-      renderData();
-    });
+  axios.get(`${_url}/todos`).then(function (response) {
+    data = response.data;
+    console.log(data);
+    renderData();
+  });
 }
 // 預設載入初始化環境
 init();
@@ -30,11 +28,9 @@ save.addEventListener("click", function (e) {
   }
   let obj = {};
   obj.content = txt.value;
-  axios
-    .post(`https://todolist-20of.onrender.com/todos`, obj)
-    .then(function (res) {
-      init();
-    });
+  axios.post(`${_url}/todos`, obj).then(function (res) {
+    init();
+  });
 });
 // 刪除待辦功能
 list.addEventListener("click", function (e) {
@@ -44,10 +40,8 @@ list.addEventListener("click", function (e) {
     return;
   }
   let num = e.target.getAttribute("data-num");
-  axios
-    .delete(`https://todolist-20of.onrender.com/todos/${num}`)
-    .then(function (res) {
-      alert("刪除成功！");
-      init();
-    });
+  axios.delete(`${_url}/todos/${num}`).then(function (res) {
+    alert("刪除成功！");
+    init();
+  });
 });
--- page.js
+++ page.js
@@ -1,10 +1,8 @@
 // http://127.0.0.1:5500/page.html?id=3
 const id = location.href.split("=")[1];
-const _url = "https://todolist-20of.onrender.com/"; // 修改處
-axios
-  .get(`https://todolist-20of.onrender.com/todos/${id}`)
-  .then(function (response) {
-    console.log(response);
-    document.querySelector("h1").textContent = response.data.id;
-    document.querySelector(".content").textContent = response.data.content;
-  });
+const _url = "https://todolist-20of.onrender.com"; // 修改處
+axios.get(`${_url}/todos/${id}`).then(function (response) {
+  console.log(response);
+  document.querySelector("h1").textContent = response.data.id;
+  document.querySelector(".content").textContent = response.data.content;
+});
