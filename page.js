6c480627eb524e42ac3624c8cdf85c37d4a30d08
Uli <ru21041555@gmail.com>
2023-10-29 21:32:33 +0800

    修改路徑

--- page.js
+++ page.js
@@ -1,6 +1,6 @@
 // http://127.0.0.1:5500/page.html?id=3
 const id = location.href.split("=")[1];
-const _url = "http://localhost:3000"; // 修改處
+const _url = "https://todolist-20of.onrender.com"; // 修改處
 axios.get(`${_url}/todos/${id}`).then(function (response) {
   console.log(response);
   document.querySelector("h1").textContent = response.data.id;
